522fce9f6bbcbcf8e9c33c4ba9ef895ca950c5be
MikeJ77 <mikejohnson3505@gmail.com>
2021-12-27 20:32:48 +0300

    Second commit

--- src/test/java/com/cydeo/tests/Test_Mike.java
+++ src/test/java/com/cydeo/tests/Test_Mike.java
@@ -3,5 +3,6 @@ package com.cydeo.tests;
 public class Test_Mike {
     public static void main(String[] args) {
         System.out.println("First Change From Mike");
+        System.out.println("Second Change From Mike");
     }
 }
